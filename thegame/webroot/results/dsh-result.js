b459203344b1d23e4749cfda595268a8d06293ac
Pollux <dan.henry@optum.com>
2018-05-15 10:46:13 -0500

    Updated with my messages

--- thegame/webroot/results/dsh-result.js
+++ thegame/webroot/results/dsh-result.js
@@ -1,5 +1,5 @@
 function Result() {
-	this.WIN  = "You... FOR THE WIN!!!!";
-	this.LOSS = "LOSER!";
+	this.WIN  = "We are the champioins, my friend... and we'll keep on Ro Sham Bo'ing .... till the end...";
+	this.LOSS = "Out of the two contestants, you took second place ... you should be proud to have been so close to 1st place.";
 	this.TIE  = "Psh - You can do better than a tie";
 }
