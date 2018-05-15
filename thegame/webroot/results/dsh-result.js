eaf1d32217a031a625d4b9ad19969192e1150c8e
Pollux <dan.henry@optum.com>
2018-05-15 10:48:58 -0500

    Escaped apostrophe

--- thegame/webroot/results/dsh-result.js
+++ thegame/webroot/results/dsh-result.js
@@ -1,5 +1,5 @@
 function Result() {
-	this.WIN  = "We are the champioins, my friend... and we'll keep on Ro Sham Bo'ing .... till the end...";
+	this.WIN  = "We are the champioins, my friend... and we'll keep on Ro Sham Bo\'ing .... till the end...";
 	this.LOSS = "Out of the two contestants, you took second place ... you should be proud to have been so close to 1st place.";
 	this.TIE  = "Psh - You can do better than a tie";
 }
